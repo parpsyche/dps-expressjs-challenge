7fbcecce69fd03f11a73664dabe5758ac1acc0ca
deepython05 <deepython05@gmail.com>
2024-07-29 06:17:16 +0530

    Valid API Endpoint Listing Inspired by Django
    Introduce path listing to improve API access. This change includes:
    - A method to list all valid API endpoints if any invalid route is accessed. Inspired from Django

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import endPoints from 'express-list-endpoints';
 import dotenv from 'dotenv';
 import projectRoute from './routes/projectRoutes';
 import reportRoute from './routes/reportRoutes';
@@ -13,7 +14,9 @@ app.use(express.json());
 app.use(authMiddleware);
 app.use('/projects', projectRoute);
 app.use('/reports', reportRoute);
-
+app.get('*', (req, res) => {
+	res.status(200).send(endPoints(app));
+});
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
